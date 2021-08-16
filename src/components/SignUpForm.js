71eb2d96eefd60c605d01c32f354260f4bad7ea8
Alex Calvert <alexanderajcalvert@gmail.com>
2021-08-16 10:41:29 -0700

    Local state for login to switch between log in and sign up form established.

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,8 +1,38 @@
+import { Provider, useDispatch, useSelector } from 'react-redux'
+import { changeUsernameInput, changePasswordInput, setLoggedInUser } from '../store/reducers/reducerSlice'
+
 function SignUpForm() {
+    const dispatch = useDispatch();
+    const usernameInput = useSelector(state => state.usernameInput)
+    const passwordInput = useSelector(state => state.passwordInput)
+
+    const handleUsernameChange = (e) => {
+        console.log(e)
+        dispatch(changeUsernameInput(e.target.value))
+        // dispatch({ type: "CHANGE_USERNAME_INPUT", payload: e.target.value })
+      }
+    
+      const handlePasswordChange = (e) => {
+        console.log(e)
+        dispatch(changePasswordInput(e.target.value))
+        // dispatch({ type: "CHANGE_PASSWORD_INPUT", payload: e.target.value })
+      }
+    
+      const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log (usernameInput, passwordInput)
+      }
 
     return (
         <>
         <h1>This is the Sign Up Form component.</h1>
+        <form onSubmit={handleSubmit}>
+            <input type="text" onChange={handleUsernameChange}>
+            </input>
+            <input type="text" onChange={handlePasswordChange}>
+            </input>
+            <input type="submit" value="Submit"></input>
+        </form>
         </>
     )
 
