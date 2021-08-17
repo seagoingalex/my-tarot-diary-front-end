4a4c5ed2f81d76bb7e89b6ba423e98cbe915ecbb
Alex Calvert <alexanderajcalvert@gmail.com>
2021-08-17 12:31:58 -0700

    UseParams for reading view AFTER creating a new reading successful.

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,7 @@ import Chart from "./Chart";
 import CardList from "./CardList";
 import Cards from "./Cards";
 import CardView from "./CardView";
+import ReadingView from "./ReadingView";
 
 function App() {
   const dispatch = useDispatch();
@@ -63,7 +64,7 @@ function App() {
         <Chart></Chart>
       </Route>
       <Route path="/chart/:id">
-        <Chart></Chart>
+        <ReadingView></ReadingView>
       </Route>
       <Route exact path="/library">
         <Cards></Cards>
--- src/components/ReadingView.js
+++ src/components/ReadingView.js
@@ -1,8 +1,27 @@
+import React, { useEffect, useState } from "react";
+import { useParams, useHistory } from "react-router-dom"
+
 function ReadingView() {
+    const [reading, setReading] = useState(null);
+    const { id } = useParams()
+
+    const history = useHistory();
+
+    useEffect(() => {
+        fetch(`http://localhost:3000/readings/${id}`)
+            .then(r => r.json())
+            .then(data => setReading(data))
+            // .then(data => console.log(data))
+    }, [id])
+
+    if (!reading) return <h2>Loading...</h2>
 
     return (
         <>
         <h1>This is the Reading View component.</h1>
+        <h1>{reading.question}</h1>
+        {/* <button onClick={() => history.goBack()}>Back</button> */}
+        
         </>
     )
 
