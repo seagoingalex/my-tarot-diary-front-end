d045016f80b4410de6b228d63f31572d6ab47c32
Alex Calvert <alexanderajcalvert@gmail.com>
2021-08-17 18:30:04 -0700

    Card data rendering on Reading page correctly.

--- src/components/ReadingView.js
+++ src/components/ReadingView.js
@@ -2,35 +2,43 @@ import React, { useEffect, useState } from "react";
 import { useParams, useHistory, Link } from "react-router-dom"
 
 function ReadingView() {
-    const [reading, setReading] = useState({ readingData: [], cards: [] });
-    const { id } = useParams()
+    const [reading, setReading] = useState([])
+    const [cards, setCards] = useState([])
+    const [card, setCard] = useState([])
 
+    const { id } = useParams()
 
     const history = useHistory();
 
     useEffect(() => {
         fetch(`http://localhost:3000/readings/${id}`)
             .then(r => r.json())
-            .then(data => setReading({ readingData: data, cards: data.cards }))
+            .then(data => {
+                setReading(data)
+                setCard(data.cards[0])
+            })
             // .then(data => setReading(data))
             // .then(data => console.log(data))
     }, [id])
 
-    if (!reading) return <h2>Loading...</h2>
+    if (!reading && !cards) return <h2>Loading...</h2>
 
     return (
         <>
         <h1>This is the Reading View component.</h1>
-        <h1>Question: {reading.readingData.question}</h1>
-        {/* <h1>You drew: {reading.cards[0].name}</h1> */}
-        <h2>Rating: {reading.readingData.rating}</h2>
-        <h2>Descriptors: {reading.readingData.descriptors}</h2>
-        <h3>Notes: {reading.readingData.notes}</h3>
-        <Link to={`/chart/${reading.readingData.id}/edit`}>
+        <h1>Question: {reading.question}</h1>
+        {/* <h1>You drew: {cards.first.name}</h1> */}
+        <h1>You drew: {card.name}</h1>
+        <h2>Rating: {reading.rating}</h2>
+        <h2>Descriptors: {reading.descriptors}</h2>
+        <h3>Notes: {reading.notes}</h3>
+        <Link to={`/chart/${reading.id}/edit`}>
             <button> Edit Entry </button>
         </Link>
         <button onClick={() => history.goBack()}>Back</button>
-        {/* <img src={reading.cards[0].img}></img> */}
+        <Link to={`/library/${card.id}`}>
+            <img src={card.img}></img>
+        </Link>
         </>
     )
 
