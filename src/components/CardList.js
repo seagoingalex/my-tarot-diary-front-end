acd80618c86de35932803797d7cfd8db57d76008
Alex Calvert <alexanderajcalvert@gmail.com>
2021-08-19 12:07:45 -0700

    Refactored library component to possess a Grid from Material UI in place of original flexbox. Added dispatch to CardList, rendering Cards component unnecessary.

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,13 +1,73 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from 'react-router-dom';
 
-function CardList({ cards = [] }) {
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import Radio from '@material-ui/core/Radio';
+import Paper from '@material-ui/core/Paper';
+
+import { fetchCards } from "../store/reducers/reducerSlice.js";
+import { useSelector, useDispatch } from "react-redux";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    height: 425.7,
+    width: 258.9,
+  },
+  control: {
+    padding: theme.spacing(2),
+  },
+}));
+
+// function CardList({ cards = [] }) {
+function CardList() {
+  const [spacing, setSpacing] = React.useState(2);
+  const classes = useStyles();
+
+  const cards = useSelector((state) => state.entities);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCards());
+  }, [dispatch]);
 
     function handleCardClick(e) {
         console.log("You clicked a card!")
         console.log(e.target)
 
     }
+
+    return (
+      <Grid container className={classes.root} spacing={2}>
+      <Grid item xs={12}>
+        <Grid container justifyContent="center" spacing={3}>
+          {cards.map((card) => (
+        // <Link to="/cardview/:id" > 
+          <Grid key={card.id} item>
+            <Link to={`/library/${card.id}`}>
+              {/* <Paper className={classes.paper}></Paper> */}
+                <img onClick={handleCardClick} className={classes.paper} key={card.id} src={card.img} alt="card" value={card.id}/>
+            </Link>
+          </Grid>
+          
+          // {[0, 1, 2].map((value) => (
+          //   <Grid key={value} item>
+          //     <Paper className={classes.paper} />
+          //   </Grid>
+          // )
+          ))}
+        </Grid>
+      </Grid>
+    </Grid>
+    );
+
 
   return (
     <div >
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,7 +3,31 @@ import { useSelector, useDispatch } from "react-redux";
 import CardList from "./CardList";
 import { fetchCards } from "../store/reducers/reducerSlice.js";
 
-function Cats() {
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import Radio from '@material-ui/core/Radio';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    height: 140,
+    width: 100,
+  },
+  control: {
+    padding: theme.spacing(2),
+  },
+}));
+
+function Cards() {
+  const [spacing, setSpacing] = React.useState(2);
+  const classes = useStyles();
+
 //   const cards = useSelector((state) => state.global.entities);
     const cards = useSelector((state) => state.entities);
 
@@ -14,6 +38,15 @@ function Cats() {
   }, [dispatch]);
 
   return (
+    // <Grid container className={classes.root} spacing={2}>
+    //   <Grid item xs={12}>
+    //     <Grid container justifyContent="center" spacing={spacing}>
+    //       <CardList cards={cards} />
+    //     </Grid>
+    //   </Grid>
+    // </Grid>
+
+
     <div className="App">
       <h1>Cards</h1>
       <div className="library-container">
@@ -23,4 +56,15 @@ function Cats() {
   );
 }
 
-export default Cats;
+
+//   return (
+//     <div className="App">
+//       <h1>Cards</h1>
+//       <div className="library-container">
+//         <CardList cards={cards} />
+//       </div>
+//     </div>
+//   );
+// }
+
+export default Cards;
